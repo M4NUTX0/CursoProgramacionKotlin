d55337f6a0c78270818a4d4e338aaf4c929e10aa
mcastroiru <mcastroiru@educacion.navarra.es>
2025-10-07 12:56:48 +0200

    Ejercicio 2 terminado

--- app/src/main/java/com/tutorialesprogramacionya/proyecto002/Proyecto002.kt
+++ app/src/main/java/com/tutorialesprogramacionya/proyecto002/Proyecto002.kt
@@ -1,6 +1,9 @@
 package com.tutorialesprogramacionya.proyecto002
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +19,15 @@ class Proyecto002 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val valor1 = findViewById<EditText>(R.id.proyecto002_et_valor1)
+        val valor2 = findViewById<EditText>(R.id.proyecto002_et_valor2)
+        val resultado = findViewById<TextView>(R.id.proyecto002_tv_resultado)
+        val btnCalcular = findViewById<Button>(R.id.proyecto002_btn_calcular)
+        btnCalcular.setOnClickListener {
+            val suma = valor1.text.toString().toInt() + valor2.text.toString().toInt()
+            resultado.text = "Resultado: " + suma
         }
     }
 
